025e7d8429362df43ee89510aba58a40d993ad6a
Navjot Singh <62318415+NavjotSingh01@users.noreply.github.com>
2022-09-01 03:26:26 -0400

    CHANGES

--- config.ts
+++ config.ts
@@ -1,9 +1,8 @@
 export const config = {
-    TRIAL: 'https://api.themoviedb.org/3/movie/popular?api_key=f5d914760bcea6a7f39d1b6d24e8df03',
     POPULAR_MOVIES_API_URL : 'https://api.themoviedb.org/3/movie/popular',
     API_URL_TOP_RATED:'https://api.themoviedb.org/3/movie/top_rated' ,
-    API_KEY : 'f5d914760bcea6a7f39d1b6d24e8df03',
-    // MOVIES_BASE_URL : 'https://api.themoviedb.org/t/p/',
+    API_KEY : '',
+    MOVIES_BASE_URL : 'https://api.themoviedb.org/t/p/',
     ORIGINAL_POSTER_BASE_URL : 'https://image.tmdb.org/t/p/original',
     POSTER_BASE_URL: 'https://image.tmdb.org/t/p/w342',
 
